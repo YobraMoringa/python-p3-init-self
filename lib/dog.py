2ad8b41e45604f1b2500cf40439fcb4485b463b2
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-06-26 14:41:18 +0300

    __init__ method added in classes

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def adopt(self, owner_name):
+        self.owner = owner_name
+
+    def showing_self(self):
+        return self
+
+fido = Dog("Fido")
+print(fido.name)
+print(fido.showing_self())
+print(fido is fido.showing_self())
+fido.owner = "Sophie"
+print(fido.owner)
+fido.adopt("Sopie")
+print(fido.owner)
+
+snoopy = Dog("snoopy")
+print(snoopy.name)
